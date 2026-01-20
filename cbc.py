9766fcd3bb30669bb601b59c452238d23319d7d6
Wiktor <wiktorklepacz@gmail.com>
2026-01-20 15:21:02 +0100

    deleted unused function

--- cbc.py
+++ cbc.py
@@ -5,11 +5,6 @@ import rsa_core
 # NOTE
 # RSA is not a block cipher and CBC mode is not used in real-world cryptosystems.
 # testing of the key generation algorithm
-if False:
-    e, d, n = keygen(8)
-    print(e)
-    print(d)
-    print(n)
 
 
 # CONVERTING THE DATA INTO BLOCKS
@@ -18,32 +13,6 @@ def generate_iv(block_size: int) -> int:
     max_val = 2 ** (block_size * 8) - 1
     return random.randint(min_val, max_val)
 
-
-def convert_msg_to_blocks(message: bytes, block_size: int) -> list[int]:
-    blocks = []
-    # pad the message
-    message = rsa_core.pad_message(message, block_size)
-
-    # split the message into blocks
-    for i in range(0, len(message), block_size):
-        # iterate over the message
-        block = message[i: i+block_size]
-        block_int = int.from_bytes(block, byteorder="big")
-        blocks.append(block_int)
-    
-    return blocks
-
-
-# testing of the padding functionality
-if False:
-    text = "Bicycle Day is an unofficial celebration on April 19th of the psychedelic revolution[1] and the first psychedelic trip on LSD by Albert Hofmann in 1943, in tandem with his bicycle ride home from Sandoz Labs.[2][3] It is commonly celebrated by ingesting psychedelics and riding a bike, sometimes in a parade,[4] and often with psychedelic-themed festivities.[5] The holiday was first named and declared in 1985 by Thomas Roberts, a psychology professor at Northern Illinois University,[6][7] but has likely been celebrated by psychedelic enthusiasts since the beginning of the psychedelic era, and celebrated in popular culture since at least 2004.[8]"
-
-    # text = "Bicycle Day (psychedelic holiday)"
-    block_size = 10
-
-    blocks = string_to_blocks(text, block_size)
-
-    decoded_text = blocks_to_string(blocks, block_size)
 
 def xor_int(a: int, b: int) -> int:
     return a ^ b
@@ -61,6 +30,7 @@ def rsa_cbc_encrypt(blocks: list[int], e: int, n: int, iv: int, block_size: int)
         prev = encrypted_block & mask # important
 
     return encrypted_blocks
+
 
 def rsa_cbc_decrypt(encrypted_blocks: list[int], d: int, n: int, iv: int, block_size: int) -> list[int]:
     blocks = []
